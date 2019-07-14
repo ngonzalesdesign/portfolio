98a3e0a12e397dc18bf70389132499b08522b32d
ngonzalesdesign <nzales18@gmail.com>
2019-07-13 22:23:08 -0600

    atf mobile styles

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,8 @@ module.exports = {
     require("precss"),
     require("postcss-import-ext-glob"),
     require("postcss-import"),
-    require("postcss-color-function"),
-    require("postcss-extend-rule")
+    require("postcss-extend-rule"),
+    require("postcss-color-mod-function"),
+    require("postcss-scale")
   ]
 };

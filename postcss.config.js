295d76d9d05a0b4644dffaa186f89338cec64255
ngonzalesdesign <nzales18@gmail.com>
2019-07-18 17:06:12 -0600

    contact page mobile layout

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,6 @@ module.exports = {
     require("postcss-import"),
     require("precss"),
     require("postcss-extend-rule"),
-    require("postcss-color-mod-function"),
-    require("postcss-scale")
+    require("postcss-color-mod-function")
   ]
 };

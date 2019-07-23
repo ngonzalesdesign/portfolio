ca5ea0efcb927e6e1b787bc866bdfd6044da00ef
ngonzalesdesign <nzales18@gmail.com>
2019-07-23 12:12:05 -0600

    added promise to form js

--- _site/_includes/assets/js/form.js
+++ _site/_includes/assets/js/form.js
@@ -1,8 +1,18 @@
+let testForm = document.querySelector("#contact");
 
-
-const myForm = document.querySelector("#contact");
-
-myForm.onsubmit = function(ev) {
-  ev.preventDefault();
-  return alert("Success!");
-}
+testForm.addEventListener("submit", e => {
+  e.preventDefault();
+  const formData = new FormData(testForm);
+  fetch(testForm.getAttribute("action"), {
+    method: "POST",
+    headers: {
+      Accept: "application/x-www-form-urlencoded;charset=UTF-8",
+      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+    },
+    body: new URLSearchParams(formData).toString()
+  }).then(res => {
+    if (res) {
+      return alert("Success!");
+    }
+  });
+});

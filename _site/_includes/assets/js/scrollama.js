2c9cfb2f43543e1a181b353cd4c10928149b8fbb
ngonzalesdesign <nzales18@gmail.com>
2019-08-24 11:28:46 -0600

    rocket animation

--- _site/_includes/assets/js/scrollama.js
+++ _site/_includes/assets/js/scrollama.js
@@ -3,20 +3,37 @@ const scroller2 = scrollama();
 const nav = document.querySelector("header");
 const strengths = document.querySelector(".strengths");
 
+function scrollToTop(response, direction) {
+  if (response.direction == direction) {
+  response.element.classList.add("is-active");
+  response.element.scrollIntoView({block: "start", behavior: "smooth"});
+  }
+};
+
+function animatePage(response){
+  var cls = response.element.classList;
+  switch( true ){
+    case cls.contains('one'):
+      strengths.classList = `strengths + step-one`;
+      nav.classList = "fixed";
+      break;
+    case cls.contains('two'):
+      strengths.classList = `strengths + step-two`
+      break;
+    case cls.contains('three'):
+      strengths.classList = `strengths + step-three`
+      break;      
+  }
+}
+
 scroller
   .setup({
     step: ".step",
     offset: 1
   })
   .onStepEnter(function(response) {
-    if (response.direction == "down") {
-      response.element.classList.add("is-active");
-      response.element.scrollIntoView({block: "start", behavior: "smooth"});
-    }
-
-    if (response.element.classList.contains("first")) {
-      nav.classList = "fixed";
-    }
+    scrollToTop(response, "down");
+    animatePage(response);
 
     if (response.element.classList.contains("strength")) {
       strengths.classList.add("fixedBG");
@@ -36,10 +53,7 @@ scroller2
     offset: 0.1
   })
   .onStepEnter(function(response) {
-    if (response.direction == "up") {
-      response.element.classList.add("is-active");
-      response.element.scrollIntoView({block: "start", behavior: "smooth"});
-    }
+    scrollToTop(response, "up");
   })
   .onStepExit(function(response) {
     if (response.direction == "up") {
--- posts/fourthpost.md
+++ posts/fourthpost.md
@@ -9,4 +9,4 @@ tags:
   - politics
 ---
 
-Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.
+Leverage agile <a href=""> to provide a robust</a> synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.
